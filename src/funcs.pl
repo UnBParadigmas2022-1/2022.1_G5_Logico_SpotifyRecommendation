d13eda3ba2d0a73ac06e2c68d023700d5a2dfaac
matheusafonsouza <matheusafonsouza@gmail.com>
2022-07-31 13:23:47 -0300

    fix: create recommendations folder and remove some comments

--- src/funcs.pl
+++ src/funcs.pl
@@ -1,7 +1,5 @@
-% Limpa a tela
 clear :- write('\e[H\e[2J').
 
-% Define um predicado switch case para validar opções do input
 switch(_, [], ReturnTo) :- write('Opção inválida'),nl, call(ReturnTo).
 switch(X, [Val:Goal|Cases], ReturnTo) :-
     ( X==Val ->
@@ -10,5 +8,4 @@ switch(X, [Val:Goal|Cases], ReturnTo) :-
         switch(X, Cases, ReturnTo)
     ).
 
-% Finaliza o programa
 exit:- write('Encerrado'),nl, halt(0).
