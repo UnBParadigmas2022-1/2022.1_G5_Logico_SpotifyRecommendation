2529c45554d6d1a8d751ae971f1d05e94f8acc26
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-07-31 16:45:11 -0300

    feat: get top tracks by popularity
    Co-authored-by: Thiago Mesquita <thiago099carvalho@gmail.com>

--- src/recommendation/popularity.pl
+++ src/recommendation/popularity.pl
@@ -23,13 +23,33 @@ menu_recommend_by_popularity :-
 
 
 recommend_by_popularity :-
-    findall(Pop-Track,
+    new(D, dialog('Recomendacoes')),
+
+    % get a pair set with Popularity and Track
+    findall(Popularity-Track,
             music(_,
                   _,
                   Track,
-                  Pop,
+                  Popularity,
                   _,
                   _),
-            PopsTracks),
-    keysort(PopsTracks, Sorted),
-    print_formated(Sorted).
+            TracksWithPopularity),
+
+    % sort pair set by Key (popularity)
+    keysort(TracksWithPopularity, SortedPair),
+
+    % get the value of pair set (tracks name)
+    pairs_values(SortedPair, SortedTracks),
+
+    % remove duplicated songs
+    remove_dups(SortedTracks, ParsedTracks),
+
+    % reverse list to get the descendent sort
+    reverse(ParsedTracks, ReversedTracks),
+  
+    % get the X firsts elements of list
+    sublist(ReversedTracks,0,10,TopTracks),
+
+    % append track to interface
+    append_track(TopTracks, D),
+    send(D, open).
